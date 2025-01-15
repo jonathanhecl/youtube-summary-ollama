81199b4bf96cd8302af9507350137242ae7aa883
Jonathan Hecl <jonathanhecl@gmail.com>
2025-01-15 20:11:07 -0300

    getTranscription

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/jonathanhecl/youtube-summary-ollama
 
 go 1.22.4
+
+require (
+	github.com/jonathanhecl/gollama v1.0.26 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var (
 	version = "0.0.1"
@@ -8,4 +10,12 @@ var (
 
 func main() {
 	fmt.Println("YouTube Summary Ollama v", version)
+
+	videoID := "TNrylG4hUIQ"
+
+	content, err := getTranscription(videoID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(content)
 }
